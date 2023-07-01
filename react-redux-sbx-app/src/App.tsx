38fd1f130662628d9da7f2a69e81f2ca9647e42d
Dylan Alvandi <g.dylanalvandi@gmail.com>
2023-07-01 03:54:37 +0200

    origin/main: Test commit from g.dylanalvandi@gmail.com via sourcetree

--- react-redux-sbx-app/src/App.tsx
+++ react-redux-sbx-app/src/App.tsx
@@ -8,9 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <ReduxBasic/>
       </header>
     </div>
